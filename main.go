7e612ad1f9cec204f3a9f5a378d1a3ec5947993b
Daniel Grossmann-Kavanagh <me@danielgk.com>
2024-05-09 18:12:35 -0700

    feat: Add Owner role for creator of workspace, limits on workspaces and users in a workspace

--- main.go
+++ main.go
@@ -8,21 +8,18 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
 
+	"github.com/pocketbase/pocketbase/models"
 	"github.com/shirou/gopsutil/v3/cpu"
 	"github.com/shirou/gopsutil/v3/mem"
 )
 
 func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal(err)
-	}
 	handler.SetupHandlerVars()
 }
 
@@ -67,6 +64,97 @@ func main() {
 		// System Statistics
 		e.Router.GET("/stats/cpu", getCPUStats)
 		e.Router.GET("/stats/ram", getRAMStats)
+
+		return nil
+	})
+
+	app.OnRecordBeforeCreateRequest("workspaces").Add(func(e *core.RecordCreateEvent) error {
+		admin, _ := e.HttpContext.Get(apis.ContextAdminKey).(*models.Admin)
+		if admin != nil {
+			return nil // ignore for admins
+		}
+
+		user_record := e.HttpContext.Get(apis.ContextRequestInfoKey).(*models.RequestInfo).AuthRecord
+		workspace_roles_collection, err := app.Dao().FindCollectionByNameOrId("workspace_roles")
+		if err != nil {
+			return err
+		}
+		owner_role, err := app.Dao().FindFirstRecordByData("roles", "name", "Owner")
+		if err != nil {
+			return fmt.Errorf("error finding role: %v", err)
+		}
+
+		var total int
+
+		// The transaction has started, so expect to find the current record in the count.
+		err = app.Dao().RecordQuery(workspace_roles_collection).
+			Select("count(*)").
+			AndWhere(dbx.HashExp{"role": owner_role.Id}).
+			AndWhere(dbx.HashExp{"user": user_record.Id}).
+			Row(&total)
+
+		if err != nil || total >= 25 {
+			return echo.NewHTTPError(http.StatusForbidden, fmt.Errorf("you can only be an Owner of 25 workspaces"))
+		}
+
+		// Set Defaults
+		e.Record.Set("user_limit", 2)
+		return nil
+	})
+
+	app.OnRecordBeforeCreateRequest("workspace_roles").Add(func(e *core.RecordCreateEvent) error {
+		//admin, _ := e.HttpContext.Get(apis.ContextAdminKey).(*models.Admin)
+		//if admin != nil {
+		//	return nil // ignore for admins
+		//}
+		user_record := e.HttpContext.Get(apis.ContextRequestInfoKey).(*models.RequestInfo).AuthRecord
+		workspace, err := app.Dao().FindFirstRecordByData("workspaces", "id", e.Record.GetString("workspace"))
+		if err != nil {
+			return err
+		}
+
+		var total int
+
+		// The transaction has started, so expect to find the current record in the count.
+		err = app.Dao().RecordQuery(e.Record.Collection()).
+			Select("count(*)").
+			AndWhere(dbx.HashExp{"workspace": e.Record.GetString("workspace")}).
+			AndWhere(dbx.HashExp{"user": user_record.Id}).
+			Row(&total)
+
+		if err != nil || total >= workspace.GetInt("user_limit") {
+			return echo.NewHTTPError(http.StatusForbidden, fmt.Errorf("you have exceeded the user limit for this workspace"))
+		}
+
+		return nil
+	})
+
+	app.OnRecordAfterCreateRequest("workspaces").Add(func(e *core.RecordCreateEvent) error {
+		// Create a workspace_role such that the creator of the workspace is now the owner.
+		admin, _ := e.HttpContext.Get(apis.ContextAdminKey).(*models.Admin)
+		if admin != nil {
+			return nil // ignore for admins
+		}
+		user_record := e.HttpContext.Get(apis.ContextRequestInfoKey).(*models.RequestInfo).AuthRecord
+
+		owner_role, err := app.Dao().FindFirstRecordByData("roles", "name", "Owner")
+		if err != nil {
+			return fmt.Errorf("error finding role: %v", err)
+		}
+
+		workspace_roles_collection, err := app.Dao().FindCollectionByNameOrId("workspace_roles")
+		if err != nil {
+			return err
+		}
+
+		workspace_role := models.NewRecord(workspace_roles_collection)
+		workspace_role.Set("workspace", e.Record.Id)
+		workspace_role.Set("user", user_record.Id)
+		workspace_role.Set("role", owner_role.Id)
+
+		if err := app.Dao().SaveRecord(workspace_role); err != nil {
+			return err
+		}
 
 		return nil
 	})
